990b9f666dc068b40416f87e189a0e8f160f9fe2
kyriri <git-kyriri@opayq.com>
2022-06-10 18:55:25 -0300

    feat: Cria produtos e exibe na tela inicial
    Co-authored-by: Maciel Júnior <macieljuniormax@users.noreply.github.com>

---
+++ spec/factories/products.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :product do
+    name { "MyString" }
+    brand { "MyString" }
+    description { "MyString" }
+    sku { "MyString" }
+  end
+end
--- spec/system/visitor_visit_homepage_spec.rb
+++ spec/system/visitor_visit_homepage_spec.rb
@@ -6,4 +6,30 @@ describe 'Visitante visita a app' do
 
     expect(page).to have_content 'E-Commerce'
   end
+
+  it 'e vê produtos' do
+    Product.create!(name: 'Caneca Mon Amour', 
+                    brand: 'TOC & Ex-TOC',
+                    description: 'Caneca em cerâmica com desenho de uma flecha do cupido',
+    )
+    Product.create!(name: 'Garrafa Star Wars', 
+                    brand: 'Zona Criativa',
+                    description: 'Garrafa térmica inox, star wars'
+    )
+    visit root_path
+
+    expect(Product.count).to be 2
+    expect(page).to have_text 'Caneca Mon Amour'
+    expect(page).to have_text 'TOC & Ex-TOC'
+    expect(page).to have_text 'Caneca em cerâmica com desenho de uma flecha do cupido'
+    expect(page).to have_text 'Garrafa Star Wars'
+    expect(page).to have_text 'Zona Criativa'
+    expect(page).to have_text 'Garrafa térmica inox, star wars'
+  end
+
+  it "e não vê produtos" do
+    visit root_path
+
+    expect(page).to have_text 'Não existem produtos cadastrados' 
+  end
 end
