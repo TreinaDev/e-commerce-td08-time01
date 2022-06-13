b52ba972b5c22b7db4025e29ada7cbe457a47cc2
kyriri <git-kyriri@opayq.com>
2022-06-13 14:22:13 -0300

    test: Corrige contexto do teste de unicidade
    Co-authored-by: Maciel Júnior <macieljuniormax@users.noreply.github.com>

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,18 +3,18 @@ require 'rails_helper'
 RSpec.describe Product, type: :model do
   describe '#valid?' do
 
-    context '#presence' do
+    context 'for presence' do
       it { should validate_presence_of(:name) }
       it { should validate_presence_of(:brand) }
       it { should validate_presence_of(:description) }
       it { should validate_presence_of(:sku) }
     end
 
-    context '#presence' do
+    context 'for uniqueness' do
       it { should validate_uniqueness_of(:sku) }
     end
 
-    context '#format' do
+    context 'for format' do
       it { should allow_value('CAT38710').for(:sku) }
       it { should allow_value('7U0330P3JD').for(:sku) }
       it { should_not allow_value('/..?').for(:sku) }
