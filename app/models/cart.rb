7afb2f485f97e4cb797587b8f076bea4c8953109
Victor de Oliveira <oliveira.victor.51200@gmail.com>
2022-06-14 13:56:48 -0300

    feat: Implementa validações e seus devidos testes unitários.
    Co-authored-by: Maciel Júnior <macieljuniormax@users.noreply.github.com>

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,4 +1,5 @@
 class Cart < ApplicationRecord
   belongs_to :product
   belongs_to :user
+  validates :quantity, presence: true, numericality: {greater_than: 0}
 end
--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Cart, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#valid?' do
+    context '#belong_to' do
+      it {should belong_to(:product)}
+      it {should belong_to(:user)}
+    end
+
+    context '#presence' do
+      it {should validate_presence_of(:quantity)}
+    end
+
+    context '#numericality' do
+      it {should validate_numericality_of(:quantity).is_greater_than(0)}
+    end
+  end
 end
