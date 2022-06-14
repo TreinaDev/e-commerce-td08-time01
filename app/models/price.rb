89261ce7a95d3dc021141b61b3784c6031b354ca
Thiago Alencar <talencar@gmail.com>
2022-06-14 11:20:51 -0300

    test: Adiciona testes unitários em Price.
    Co-authored-by: kyriri <kyriri@users.noreply.github.com>

--- app/models/price.rb
+++ app/models/price.rb
@@ -1,3 +1,16 @@
 class Price < ApplicationRecord
+  validates :price_in_brl, :validity_start, presence: true
+  validates :price_in_brl, numericality: { greater_than: 0 }
+  validates :validity_start, uniqueness: { scope: :product_id, message: 'já está cadastrada em outra instância de Price para este produto' }
+  validate :validity_start_is_future
+  
   belongs_to :product
+
+  private
+
+  def validity_start_is_future
+    if validity_start.present? && validity_start < Date.today
+      errors.add(:validity_start, 'deve ser depois de agora.')
+    end
+  end
 end
--- spec/models/price_spec.rb
+++ spec/models/price_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Price, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context '#valid?' do
+    it { should belong_to :product }
+    it { should validate_presence_of :price_in_brl}
+    it { should validate_presence_of :validity_start}
+    it { should validate_numericality_of(:price_in_brl).is_greater_than(0) }
+    
+    it do
+      FactoryBot.create(:price)
+      should validate_uniqueness_of(:validity_start)
+      .scoped_to(:product_id)
+      .with_message('já está cadastrada em outra instância de Price para este produto')
+    end
+
+    it 'data estimada de entrega não pode ser retroativa' do
+      price = Price.new(validity_start: 1.day.ago)
+
+      price.valid?
+
+      expect(price.errors.include?(:validity_start)).to be true
+      expect(price.errors[:validity_start]).to include('deve ser depois de agora.')
+    end
+
+  end
 end
