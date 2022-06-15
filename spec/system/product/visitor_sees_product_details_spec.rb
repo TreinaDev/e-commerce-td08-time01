6ec17177b28a6cc37fa6a7e68f38c9ce8e42c246
kyriri <31296669+kyriri@users.noreply.github.com>
2022-06-14 21:31:46 -0300

    refactor: Independiza resultado da velocidade de processamento
    Como existe uma condição de que o início da validade de um preço não pode
    estar no passado, ao usar Time.current às vezes esse valor já está no
    momento de salvar a nova instância.
    
    Co-authored-by: Thiago Alencar <talencar@gmail.com>

--- spec/system/product/visitor_sees_product_details_spec.rb
+++ spec/system/product/visitor_sees_product_details_spec.rb
@@ -1,19 +1,16 @@
 require 'rails_helper'
 
-describe "Visitante acessa página de detalhes de um produto" do
-  it "com sucesso" do
+describe 'Unlogged user visits show of a Product' do
+  it 'succesfully' do
     product = Product.create!(name: 'Caneca Mon Amour', 
                               brand: 'TOC & Ex-TOC',
                               description: 'Caneca em cerâmica com desenho de uma flecha do cupido',
                               sku: 'TOCCAN1234')
-
-  
     Timecop.freeze(1.month.ago) do
-      Price.create(product: product, price_in_brl: 12, validity_start: Time.current)
-      Price.create(product: product,  price_in_brl: 14.99, validity_start: 28.days.from_now)
+      Price.create!(product: product, price_in_brl: 12, validity_start: 2.seconds.from_now)
+      Price.create!(product: product,  price_in_brl: 14.99, validity_start: 28.days.from_now)
     end
-  
-    Price.create(product: product, price_in_brl: 18, validity_start: 2.days.from_now)
+    Price.create!(product: product, price_in_brl: 18, validity_start: 2.days.from_now)
 
     visit root_path
     click_on 'Caneca Mon Amour'
@@ -25,7 +22,7 @@ describe "Visitante acessa página de detalhes de um produto" do
     expect(page).to have_text 'R$ 14,99' 
   end
 
-  it "mas o produto não existe" do
+  it 'but is redirected to homepage because the product does not exists' do
     visit product_path(1)
     
     expect(current_path).to eq root_path
