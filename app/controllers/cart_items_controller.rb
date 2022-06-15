50c23bbedb2210bd10a6efc4cc7cef6dbe07b586
Victor de Oliveira <oliveira.victor.51200@gmail.com>
2022-06-15 11:03:34 -0300

    style: Modifica posição de linhas e nomes de variáveis no controller de items de carrinho.
    Modifica ternário no show de produto.
    
    Retira blocos de context no "spec/models/cart_item_spec.rb".
    
    Co-authored-by: Maciel Júnior <macieljuniormax@users.noreply.github.com>

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,5 +1,9 @@
 class CartItemsController < ApplicationController
   before_action :get_user_id, except: :destroy
+  
+  def index
+    @cart = CartItem.where(user_id: @user_id)
+  end
 
   def create 
     product_id = params[:product_id]
@@ -8,21 +12,17 @@ class CartItemsController < ApplicationController
     return redirect_to new_user_session_path, notice: 'Bem vindo! Por favor registre-se ou entre em sua conta para continuar.' if user_signed_in? == false
     return redirect_to product_path(product_id), notice: 'Só é possível a compra de um ou mais produtos.' if quantity.to_i < 1
     
-    cart_line = CartItem.create!(product_id: product_id, user_id: @user_id, quantity: quantity)
+    cart_item = CartItem.create!(product_id: product_id, user_id: @user_id, quantity: quantity)
 
     redirect_to product_path(product_id), notice: 'Produto adicionado com sucesso.'
   end
 
-  def index
-    @cart = CartItem.where(user_id: @user_id)
-  end
-
   def destroy
-    cart_to_destroy = CartItem.find(params[:id])
-    user_id = cart_to_destroy.user_id
-    product_name = cart_to_destroy.product.name
+    item_to_destroy = CartItem.find(params[:id])
+    user_id = item_to_destroy.user_id
+    product_name = item_to_destroy.product.name
 
-    cart_to_destroy.destroy
+    item_to_destroy.destroy
 
     redirect_to user_cart_items_path(user_id), notice: "Produto retirado: #{product_name}."
   end
--- spec/models/cart_item_spec.rb
+++ spec/models/cart_item_spec.rb
@@ -2,17 +2,9 @@ require 'rails_helper'
 
 RSpec.describe CartItem, type: :model do
   describe '#valid?' do
-    context '#association' do
-      it {should belong_to(:product)}
-      it {should belong_to(:user)}
-    end
-
-    context '#presence' do
-      it {should validate_presence_of(:quantity)}
-    end
-
-    context '#numericality' do
-      it {should validate_numericality_of(:quantity).is_greater_than(0)}
-    end
+    it {should belong_to(:product)}
+    it {should belong_to(:user)}
+    it {should validate_presence_of(:quantity)}
+    it {should validate_numericality_of(:quantity).is_greater_than(0)}
   end
 end
