7e03e10ea087ef976748933c76bcfd185c5dd7b1
Victor de Oliveira <oliveira.victor.51200@gmail.com>
2022-06-17 11:04:08 -0300

    feat: Adiciona validações dos pedidos e seus testes.
    Co-authored-by: Maciel Júnior <macieljuniormax@users.noreply.github.com>

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,8 @@
 class Order < ApplicationRecord
   belongs_to :user
   has_many :cart_items
+  validates_presence_of :address
+  validate :must_have_cart
 
   before_create :set_code
   after_create :process_cart
@@ -22,5 +24,9 @@ class Order < ApplicationRecord
                                      .last
                                      .price_in_brl)
     end
+  end
+
+  def must_have_cart
+    return errors.add(:pedido, 'deve possuir carrinho.') if CartItem.where(order_id: nil, user: self.user).empty?
   end
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#valid?' do
+    it {should belong_to(:user)}
+    it {should have_many(:cart_items)}
+    it {should validate_presence_of(:address)}
+
+    context 'regarding the cart' do
+      it "must have a cart" do
+        order = build(:order)
+
+        expect(order.valid?).to be false
+        expect(order.errors.full_messages).to include 'Pedido deve possuir carrinho.' 
+      end
+
+      it "has a cart" do
+        user = create(:user)
+        product = create(:product)
+        create(:price, product: product)
+        create(:cart_item, user: user, product: product)
+        order = build(:order, user: user)
+
+        expect(order.valid?).to be true
+      end
+    end
+  end
 end
