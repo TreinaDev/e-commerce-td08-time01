681e31a96841f70a1b27a9f52170e5d3cd82d9bf
kyriri <31296669+kyriri@users.noreply.github.com>
2022-06-18 20:21:43 -0300

    feat: Amplia seeds de produtos
    Para melhorar a visualização dos resultados das buscas
    
    Co-authored-by: Thiago Alencar <talencar@gmail.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,43 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-p 'Create products and prices'
+puts "\n---- cria um monte de cacarecos  ------"
+
+names_and_descriptions = [
+  # ['', ''], 
+  ['Caneca Sabre de Luz', 'Caneca em cerâmica com desenho de sabre de luz que se acende quando a caneca está quente.'],
+  ['Xícara Levitadora', 'Par de duas xícaras em vidro transparente com parede dupla. Parece que seu café está flutuando!'],
+  ['Conjunto Tóquio', 'Conjunto para chá em estilo japonês, feito inteiramente em ecoplástico e composto de 4 xícaras pequenas sem alça.'],
+  ['Pano das Galáxias', 'Pano de prato 100% algodão, na cor preta com desenhos de estrelas e cometas.'],
+  ['Caneca-bolo', 'Caneca em cerâmica com impressão de receita de bolo de caneca. Acompanha sachê com mistura para bolo, basta acrescentar água.'],
+  ['Copo Sujo', 'Conjunto de 4 copos de 200ml iguaizinhos ao do boteco da esquina.'],
+  ['Copo Sujo Super Power', 'Conjunto de 2 copos como os do boteco da esquina, mas de 400ml.'],
+  ['Protetor de mesa Bar do Lado', 'Conjunto de 5 protetores de mesa, também conhecidos como coasters, com impressões de rótulos de cervejas famosas'],
+  ['Copo Colapsável EcoOffice', 'Nunca mais fique sem copo no bebedouro de galão! Este copo de 300ml se contrai a um disco que cabe em qualquer bolso. Aproveite esta novidade!'],
+  ['Codando Loucamente', 'Caneca com os dizeres "Talking is cheap, show me the code" em formatação de código'], 
+  ['Adesivos Grumpy Bear', 'Cartela de adesivos do Brown, o urso rabugento mais fofo que você conhece, e seu novo contatinho Cony a Coelha. Mas não conte pra ninguém que eles estão saindo, ainda é segredo!'], 
+  ['Fancy Napkings Silk Edition', 'Um lindo lenço branco rendado, em estilo vintage'], 
+  ['Love Book - A Luz', 'Fofíssima luminária led para livros, com garra para prender na capa, em forma de coração (que você nunca vai usar mesmo porque o último livro que você leu foi para prestar vestibular)'], 
+  ['Pires Chá das 5', 'Pires de reposição para a Xícara Chá das 5'], 
+  ['Xícara Chá das 5', 'Elegante xícara em fina cerâmica, para momentos elegantes ao tomar chá ou café.'], 
+  ['Moonbucks', 'No melhor estilo das grandes lojas de café, este copo para bebidas quentes tem uma tampa de pequeno orifício, que permite tomar café em movimento sem se sujar.'], 
+  ['Caneca Coffee Lovers', 'Eu moo, tu fazes o café, nós tomamos! Esta caneca não pode faltar na sua mesa de café da manhã!'], 
+  ['Caneca Térmica Hot Forever', 'Linda caneca térmica em aço inox, para compor um belo ambiente de trabalho na sua mesa de home office e ser ostentada furtivamente durante suas vídeo-conferências'], 
+]
+
+brands = ['Pensaminarium', 'Vesúvia', 'TOC & ex-TOC']
+
+names_and_descriptions.each do | pair |
+  Product.create(status: 'on_shelf',
+                name: pair[0], 
+                description: pair[1], 
+                brand: brands[rand(brands.size)],
+                sku: ('a'..'z').to_a.shuffle[0..1].join.upcase + (SecureRandom.random_number * 10**7).to_i.to_s,
+  ).set_price(14 * (1 + rand(100)/100.0).truncate(2))
+end
+
+puts '----- cria mais produtos e preços -----'
+
 product1 = Product.create!(status: 'on_shelf',
   name: 'Caneca Mon Amour', brand: 'TOC & Ex-TOC', sku: 'TOC1234',
   description: 'Caneca em cerâmica com desenho de uma flecha do cupido').set_price(10)
@@ -26,12 +62,15 @@ product5 = Product.create!(status: 'on_shelf',
   name: 'Camisa Large Sea', sku: 'VES2321',
   brand: 'Vestil',description: 'Camisa de algodão com estampa do mar com ondas.').set_price(89)
 
-p 'Create log-ins'
+
+puts '----- cria cadastros de usuários ------'
+
 Admin.create(email: 'claudia@mercadores.com.br', password: '123456', name: 'Claudia Ferreira')
 admin = Admin.create(email: 'manoel@mercadores.com.br', password: '123456', name: 'Manoel da Silva')
 user = User.create(email: 'joaquim@meuemail.com.br', password: '123456', name: 'Joaquim Santos')
 
-p 'Create Product categories'
+puts '----- cria categorias de produtos -----'
+
 eletronicos = ProductCategory.create(name: "Eletrônicos")
 informatica = ProductCategory.create(name: "Informática", parent: eletronicos)
 notebooks = ProductCategory.create(name: "Notebooks", parent: informatica)
@@ -50,18 +89,20 @@ moveis = ProductCategory.create(name: "Móveis")
 mesa_escritorio = ProductCategory.create(name: "Mesa de Escritório", parent: moveis)
 guarda_roupa = ProductCategory.create(name: "Guarda-roupa", parent: moveis)
 
-p 'Create Carts and Orders'
+puts '----------- cria pedidos --------------'
+
 CartItem.create!(product: product1, quantity: 5, user: user )
 CartItem.create!(product: product2, quantity: 7, user: user )
 Order.create!(address: 'Rua da entrega, 75', user: user)
 CartItem.create!(product: product3, quantity: 5, user: user )
 CartItem.create!(product: product1, quantity: 7, user: user )
 
-p 'Sumário'
-p "Foram criados #{Admin.count} admins"
-p "Foram criados #{User.count} usuários"
-p "Foram criados #{Product.count} produtos"
-p "Foram criados um total de #{Price.count} preços para #{Price.select(:product_id).distinct.count} produtos"
-p "Foram criadas #{ProductCategory.count} categorias de produtos"
-p "Foram criadas #{CartItem.count} instâncias de item de carrinho"
-p "Foram criadas #{Order.count} instâncias de pedidos"
+
+puts "\nSumário"
+puts "Foram criados #{Admin.count} admins"
+puts "Foram criados #{User.count} cadastros de clientes"
+puts "Foram criados #{Product.count} produtos"
+puts "Foram criados um total de #{Price.count} preços para #{Price.select(:product_id).distinct.count} produtos"
+puts "Foram criadas #{ProductCategory.count} categorias de produtos"
+puts "Foram colocados #{CartItem.count} itens em carrinhos"
+puts "Foram criados #{Order.count} pedidos"
