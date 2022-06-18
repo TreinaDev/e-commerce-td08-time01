d6f8b1f84b6ea642fbee4bffb4b8a505bcc764b4
kyriri <31296669+kyriri@users.noreply.github.com>
2022-06-18 11:27:24 -0300

    refactor: Move método para fora de ApplicationService
    Para manter a coerência com a estrutura do Rails.
    
    Co-authored-by: Thiago Alencar <talencar@gmail.com>

--- app/services/application_service.rb
+++ app/services/application_service.rb
@@ -1,5 +1,2 @@
 class ApplicationService
-  def self.process_cart(*args, &blocks)
-    new(*args, &blocks).process_cart
-  end
 end
---
+++ app/services/payment.rb
@@ -0,0 +1,5 @@
+class Payment < ApplicationService 
+  def self.process_cart(*args, &blocks)
+    new(*args, &blocks).process_cart
+  end
+end
