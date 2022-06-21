2c8ff166e40346889c65fd900f1d78abc181e3ef
Thiago Alencar <talencar@gmail.com>
2022-06-21 17:49:39 -0300

    test/Implementa testes de CNPJ.
    Co-authored-by: Maciel Júnior <macieljuniormax@users.noreply.github.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,15 +7,38 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i, message: ' inválido.' }
   validates :email, uniqueness: true
+
   validate :valid_cpf_cnpj
 
 
   private
 
   def valid_cpf_cnpj(cpf_cnpj = self.identify_number)
-    if !CPF.valid?(cpf_cnpj)
-      errors.add(:identify_number, "inválido.")
+    if cpf_cnpj.length == 11
+      errors.add(:identify_number, "inválido.") if !CPF.valid?(cpf_cnpj)
+    elsif cpf_cnpj.length == 14
+      errors.add(:identify_number, "inválido.") if !CNPJ.valid?(cpf_cnpj)
+    else
+      errors.add(:identify_number, "precisa ter 11 ou 14 caracteres.")
     end
   end
+  
+
+ 
+  
+ #  def valid_cpf(cpf_cnpj = self.identify_number)
+ #    if !CPF.valid?(cpf_cnpj)
+ #      errors.add(:identify_number, "inválido.")
+ #    end
+ #  end
+ #
+ #  def cnpj_validator
+ #    if CNPJ.valid?(cnpj, strict: true) == true
+ #      register_id = CNPJ.new(cnpj)
+ #      self.cnpj = register_id.formatted
+ #    else
+ #      errors.add(:cnpj, 'não é válido')
+ #    end
+ #  end
 
 end
--- spec/system/authentication/user_create_account_spec.rb
+++ spec/system/authentication/user_create_account_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe 'Someone tries to create a costumer account' do
-  it 'and is succesful' do
+  it 'and it is succesfull when CPF is valid' do
     cpf = '69907388041'
 
     visit root_path
@@ -20,7 +20,42 @@ describe 'Someone tries to create a costumer account' do
     expect(User.last.identify_number).to eq '69907388041'
   end
 
+  it 'and it is succesfull when CNPJ is valid' do
+    cnpj = '42104868000181'
+
+    visit root_path
+    click_on 'Registrar'
+    fill_in 'Nome', with: 'Cláudio Roberto'
+    fill_in 'E-mail', with: 'claudio@meuemail.com'
+    fill_in 'CPF/CNPJ', with: cnpj
+    fill_in 'Senha', with: '123456'
+    fill_in 'Confirmar senha', with: '123456'
+    click_on 'Cadastrar-se'
+
+    expect(current_path).to eq root_path
+    expect(page).to have_content 'Cláudio Roberto'
+    expect(page).to have_content 'Bem vindo! Você realizou seu registro com sucesso.'
+    expect(User.count).to eq 1
+    expect(User.last.identify_number).to eq '42104868000181'
+  end
+
   it 'and fail when cpf is invalid' do
+    cnpj = '42104868000182'
+
+    visit root_path
+    click_on 'Registrar'
+    fill_in 'Nome', with: 'Cláudio Roberto'
+    fill_in 'E-mail', with: 'claudio@meuemail.com'
+    fill_in 'CPF/CNPJ', with: cnpj
+    fill_in 'Senha', with: '123456'
+    fill_in 'Confirmar senha', with: '123456'
+    click_on 'Cadastrar-se'
+
+    expect(current_path).to eq users_path
+    expect(page).to have_content 'CPF/CNPJ inválido.'
+  end
+
+  it 'and fail when cnpj is invalid' do
     cpf = '12345678910'
 
     visit root_path
