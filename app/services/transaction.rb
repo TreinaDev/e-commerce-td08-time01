228b9aff5024d9fdc45cd64eb3ec9d511e767eab
kyriri <31296669+kyriri@users.noreply.github.com>
2022-06-26 17:55:56 -0300

    feat: Persiste código de transação relativo a um Pedido

--- app/services/transaction.rb
+++ app/services/transaction.rb
@@ -1,22 +1,23 @@
 class Transaction < ApplicationService 
   API_ROOT = 'http://localhost:4000'
   API_ENDPOINT = '/api/v1/client_transactions'
-  API_VARIABLE_HOLDING_TRANSACTION_TYPE = 'type_transaction'
+  API_VARIABLE_HOLDING_TRANSACTION_CODE = 'transaction_code'
   TRANSACTION_TYPES = ['buy_rubys', 'transaction_order', 'cashback']
 
   attr_reader :response
 
-  def initialize(user_tax_number:, value:, transaction_type:)
+  def initialize(user_tax_number:, value:, transaction_type:, order_id: nil)
       @user_tax_number = user_tax_number
+      @order_id = order_id
       @value = value
       @transaction_type = transaction_type
   end
 
-  def self.request(user_tax_number:, value:, transaction_type:)
+  def self.request(user_tax_number:, value:, transaction_type:, order_id: nil)
     object = new(user_tax_number: user_tax_number, 
-                  value: value, 
-                  transaction_type: transaction_type
-                )
+                 order_id: order_id, 
+                 value: value, 
+                 transaction_type: transaction_type)
     object.request
     object
   end
@@ -28,5 +29,8 @@ class Transaction < ApplicationService
                                         "type_transaction": @transaction_type }
                 }.as_json
     @response = Faraday.post(API_ROOT.concat(API_ENDPOINT), payload)
+    if @response.status == 201 && @order_id.present?
+      Order.find(@order_id).update!(transaction_code: JSON.parse(@response.body)[API_VARIABLE_HOLDING_TRANSACTION_CODE])
+    end
   end
 end
--- spec/services/transaction_spec.rb
+++ spec/services/transaction_spec.rb
@@ -16,15 +16,20 @@ RSpec.describe Transaction, type: :feature do
       expect(transaction.response.body).to include 'transaction_code'
     end
 
-    it 'should persist the transaction code of a Order' do
+    it 'should persist the transaction code of an Order' do
+      fake_response = double('faraday_response', status: 201, 
+                                                 body: '{ "transaction_code": "nsurg745n" }')
+      allow(Faraday).to receive(:post).and_return(fake_response)
       user = create(:user)
       create(:cart_item, user: user)
-      order = create(:order, user: user)
+      create(:order, user: user)
+
       Transaction.request(user_tax_number: '06001818398',
+                          order_id: Order.last.id,
                           value: 356,
                           transaction_type: 'transaction_order')
     
-      expect(order.transaction_code).to eq 'nsurg745n'
+      expect(Order.last.transaction_code).to eq 'nsurg745n'
     end
   end
 end
