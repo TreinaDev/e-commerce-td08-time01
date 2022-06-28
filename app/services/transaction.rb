d5dfa49530fb1ab87ca4e0f06472ec31260f622d
Thiago Alencar <talencar@gmail.com>
2022-06-28 18:22:55 -0300

    fix: Corrige chamada do método build_payload
    Co-authored-by: Júnior Beto <b-sep@users.noreply.github.com>
    Co-authored-by: kyriri <kyriri@users.noreply.github.com>

--- app/services/transaction.rb
+++ app/services/transaction.rb
@@ -39,10 +39,11 @@ class Transaction < ApplicationService
   end
 
   def request
-    @response = Faraday.post(API_ROOT + API_ENDPOINT, payload)
+    @response = Faraday.post(API_ROOT + API_ENDPOINT, build_payload)
     if @response.status == 201 && @order_id.present?
       Order.find(@order_id).update!(transaction_code: JSON.parse(@response.body)[API_VARIABLE_HOLDING_TRANSACTION_CODE])
     end
+    p "Resposta do Pagamento: #{@response.body}"
     @response
   end
 end
