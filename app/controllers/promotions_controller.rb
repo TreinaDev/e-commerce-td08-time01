e5016ef43c2a88f63717c028922277ac1942fc00
Victor de Oliveira <oliveira.victor.51200@gmail.com>
2022-06-29 14:55:37 -0300

    feat: Testa um caso de falha no registro da promoção
    Co-authored-by: Maciel Júnior <macieljuniormax@users.noreply.github.com>

--- app/controllers/promotions_controller.rb
+++ app/controllers/promotions_controller.rb
@@ -1,29 +1,36 @@
 class PromotionsController < ApplicationController
-  def index
-    @promotions = Promotion.all
-    @new_promotion = Promotion.new
-    @promotion_category = PromotionCategory.new
-    @categories = ProductCategory.all
-    @promotion_category = PromotionCategory.all
-  end
+  before_action :index_variables, except: [:show]
 
+  def index
+    @promotion = Promotion.new
+  end
+  
   def create 
     parameters = params.require(:promotion).permit(:name, :start_date, :end_date, :discount_percent, 
                                                    :maximum_discount, :absolute_discount_uses)
     categories_list = params[:promotion][:product_category_ids]
-
-    promotion = Promotion.new(parameters)
     
-    if PromotionsManager::CategoriesListValidator.call(categories_list) && promotion.save  
-      PromotionsManager::PromotionCategoryCreator.call(promotion, categories_list)
-      redirect_to promotion_path(promotion.id), notice: 'Promoção criada com sucesso.'
+    @promotion = Promotion.new(parameters)
+    
+    if PromotionsManager::CategoriesListValidator.call(categories_list) && @promotion.save  
+      PromotionsManager::PromotionCategoryCreator.call(@promotion, categories_list)
+      redirect_to promotion_path(@promotion.id), notice: 'Promoção criada com sucesso.'
     else
-    #   eenlk
+      error_details = @promotion.errors.full_messages.join(', ').downcase
+      flash[:alert] = "Promoção não pôde ser criada: #{error_details}"
+      render 'index'
     end
   end
-
+  
   def show
     @promotion = Promotion.find(params[:id])
     @categories = @promotion.product_categories
+  end
+  
+  private
+  
+  def index_variables
+    @all_promotions = Promotion.all
+    @categories = ProductCategory.all
   end
 end
