3fe3fdf751d76bc1c8740f7941309d6224e0fd44
Victor de Oliveira <oliveira.victor.51200@gmail.com>
2022-06-29 12:36:06 -0300

    feat: Implementa validações e seus devidos testes ao model de promoções
    Co-authored-by: Maciel Júnior <macieljuniormax@users.noreply.github.com>

--- app/models/promotion.rb
+++ app/models/promotion.rb
@@ -1,6 +1,10 @@
 class Promotion < ApplicationRecord
   has_many :promotion_categories 
   has_many :product_categories, through: :promotion_categories
+  validates_presence_of :start_date, :end_date, :name, :discount_percent, :maximum_discount, :absolute_discount_uses
+  validates :discount_percent, :maximum_discount, :absolute_discount_uses, numericality: { greater_than: 0 }
+  validates :discount_percent, numericality: { less_than: 101 }
+  validate :start_date_must_not_be_in_the_past, :end_date_must_not_be_earlier_than_start_date
 
   before_create :set_code
 
@@ -8,5 +12,17 @@ class Promotion < ApplicationRecord
 
   def set_code
     self.code = SecureRandom.alphanumeric(8)
+  end
+
+  def start_date_must_not_be_in_the_past
+    if start_date.present? && start_date < 2.seconds.ago
+      errors.add(:start_date, 'não pode estar no passado')
+    end
+  end
+
+  def end_date_must_not_be_earlier_than_start_date
+    if end_date.present? && end_date < start_date
+      errors.add(:end_date, 'não pode ser anterior à data de início')
+    end
   end
 end
