2aad3df2bf8f511fb61e89ac4ac085905fd4529e
Júnior <b.sep@live.com>
2022-06-29 11:40:11 -0300

    feat: Consulta saldo do client na api e mostra na tela

---
+++ spec/helpers/client_bonus_helper_spec.rb
@@ -0,0 +1,4 @@
+require 'rails_helper'
+
+RSpec.describe ClientBalanceHelper, type: :helper do
+end
---
+++ spec/system/user/user_see_balance_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe 'user sign in' do
+  it ' and see his balance' do
+    json_data = File.read(Rails.root.join('spec/support/json/client_data.json'))
+    fake_response = double('faraday_instance', status: 200, body: json_data)
+    user = create(:user, identify_number: '06001818398')
+
+    identify_number = { "registration_number": "06001818398" }
+
+    allow(Faraday).to receive(:get).with('http://localhost:4000/api/v1/clients_info', identify_number.as_json).and_return(fake_response)
+
+    login_as(user, scope: :user)
+    visit root_path
+
+    within('nav') do
+      expect(page).to have_content 'RB$ 1000'
+    end
+  end
+end
