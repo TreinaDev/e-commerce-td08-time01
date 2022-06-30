e0605259415eb24d9377b080bbb6a83ec5ff83d2
kyriri <31296669+kyriri@users.noreply.github.com>
2022-06-30 11:25:28 -0300

    perf: Salva apenas uma taxa de câmbio para simplificar query

--- app/models/exchange_rate.rb
+++ app/models/exchange_rate.rb
@@ -9,18 +9,17 @@ class ExchangeRate < ApplicationRecord
   validates :registered_at_source_for, presence: true
 
   def self.current
-    raise NoExchangeRateError, 'Não há nenhum taxa de câmbio disponível' unless ExchangeRate.where('registered_at_source_for <= ?', DateTime.current).present?
-    
-    ExchangeRate.where('registered_at_source_for <= ?', DateTime.current)
-                .order(registered_at_source_for: :asc)
-                .last
-                .rate
+    begin
+      ExchangeRate.take.rate
+    rescue NoMethodError
+      raise NoExchangeRateError, 'Não há nenhuma taxa de câmbio disponível'
+    end
   end
 
   def self.get
     response = Faraday.get("#{API_ROOT}/api/v1/exchange_rates/search/",
                            { "register_date": "#{I18n.l Date.current, format: :api_query}" })
-    ExchangeRate.create!(rate: JSON.parse(response.body)[API_VARIABLE_HOLDING_EXCHANGE_RATE],
+    ExchangeRate.update!(rate: JSON.parse(response.body)[API_VARIABLE_HOLDING_EXCHANGE_RATE],
                          registered_at_source_for: JSON.parse(response.body)[API_VARIABLE_HOLDING_DATE])
   end
 end
