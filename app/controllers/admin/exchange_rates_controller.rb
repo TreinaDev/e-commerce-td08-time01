ad27799394d4f5b8323b916f4966f9e415f474c5
kyriri <31296669+kyriri@users.noreply.github.com>
2022-06-30 20:45:25 -0300

    style: Remove comentário

--- app/controllers/admin/exchange_rates_controller.rb
+++ app/controllers/admin/exchange_rates_controller.rb
@@ -2,14 +2,13 @@ class Admin::ExchangeRatesController < ApplicationController
   before_action :authenticate_admin!
 
   def index
-    # checnk this below is the same as rate.current
     @current_rate = ExchangeRate.take
   end
 
   def collect_rate
     old_rate = ExchangeRate.current
     if ExchangeRate.get
-      if old_rate == ExchangeRate.current # before update is the same as after update
+      if old_rate == ExchangeRate.current # if rate before update is the same as after update
         redirect_to admin_exchange_rates_path, notice: 'A taxa de câmbio foi atualizada, mas o seu valor continua o mesmo'
       else
         redirect_to admin_exchange_rates_path, notice: "Taxa atualizada de #{old_rate} para #{ExchangeRate.current}"
