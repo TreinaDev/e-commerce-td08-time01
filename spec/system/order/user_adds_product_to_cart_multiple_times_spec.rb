d7a07c0a72399471505fe60662f7ffb49c0b67f0
kyriri <31296669+kyriri@users.noreply.github.com>
2022-07-01 02:33:22 -0300

    fix: Aumenta quantidade ao adicionar mesmo produto ao carrinho
    Antes o produto aparecia duplicado

--- spec/system/order/user_adds_product_to_cart_multiple_times_spec.rb
+++ spec/system/order/user_adds_product_to_cart_multiple_times_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-describe 'User puts item on cart' do
-  it 'from homepage' do
+describe 'User adds same product to cart twice' do
+  it 'and it is not repeated' do
     create(:exchange_rate, rate: 2)
     product_1 = create(:product, name: 'Caneca', status: 'on_shelf').set_brl_price(12)
     product_2 = create(:product, name: 'Garrafa', status: 'on_shelf').set_brl_price(5)
@@ -10,11 +10,10 @@ describe 'User puts item on cart' do
     login_as(user, scope: :user)
     visit root_path
     find("input[id='put_on_cart_#{product_2.id}']").click # Colocar no carrinho
-    cart_items = CartItem.where(user_id: user.id, order_id: nil)
+    find("input[id='put_on_cart_#{product_2.id}']").click # Colocar no carrinho
+    find("input[id='put_on_cart_#{product_2.id}']").click # Colocar no carrinho
+    visit user_cart_items_path(user)
 
-    expect(cart_items.size).to be 1
-    expect(cart_items.last.product.name).to eq 'Garrafa'
-    expect(cart_items.last.quantity).to be 1
-    expect(current_path).to eq root_path
+    expect(page).to have_text 'Garrafa 3'
   end
 end
