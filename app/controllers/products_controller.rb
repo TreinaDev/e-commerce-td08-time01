7d3555edcc4b884ed8256aee4ec5e9a530353bb4
Thiago Alencar <talencar@gmail.com>
2022-07-01 20:02:49 -0300

    fix: Impede que o usuário faça uma busca com o prompt 'Categoria'

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,11 +6,10 @@ class ProductsController < ApplicationController
   end
 
   def search
-    redirect_to root_path if params['query'] == nil
     if params['query'].present?
       @products = Search.new(params[:query]).inside_products
       @products = @products.filter(&:on_shelf?) unless admin_signed_in? || @products.empty?
-      @message_if_empty = "Nenhum resultado encontrado para: #{params[:query]}"
+      @message_if_empty = "Nenhum resultado encontrado para: #{ params[:query] }"
       render 'home/index'
     else
       @products = ProductCategory.find_by(name: params['name']).products.sort_by(&:name)
